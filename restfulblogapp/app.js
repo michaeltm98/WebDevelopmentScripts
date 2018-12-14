b7220f174b78d84c76e711ed93f782ddc0ec11df
Michael Madden <michaeltm98@gmail.con>
2018-12-14 00:12:18 -0500

    restful blog app finished with simple REST operations

--- restfulblogapp/app.js
+++ restfulblogapp/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const expressSanitizer = require("express-sanitizer");
 
 // APP config
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+//must come after app.use body parser
+app.use(expressSanitizer());
 
 // mongoose/model config
 const blogSchema = new mongoose.Schema({
@@ -43,6 +49,10 @@ app.get("/blogs/new", function(req, res) {
 })
 
 app.post('/blogs', function(req, res) {
+
+    console.log(req.body);
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    console.log(req.body);
     Blog.create(req.body.blog, function(err, newBlog) {
         if(err) {
             res.render("new");
@@ -50,9 +60,50 @@ app.post('/blogs', function(req, res) {
         else {
             res.redirect('/blogs')
         }
-    })
-})
+    });
+});
 
+app.get('/blogs/:id', function(req, res) {
+    Blog.findById(req.params.id, function(err, blog) {
+        if(err) {
+            res.redirect("index");
+        } else {
+            res.render("show", {blog: blog});
+        }
+    });
+});
+
+app.get('/blogs/:id/edit', function(req, res) {
+    Blog.findById(req.params.id, function(err, blog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: blog});
+        }
+    });
+});
+
+app.put('/blogs/:id', function(req, res) {
+    
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, blog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+app.delete('/blogs/:id', function(req, res) {
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen('3000', function() {
     console.log('Server is listening on port 3000')
