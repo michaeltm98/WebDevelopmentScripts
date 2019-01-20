c66035af743e9f2c062744a958aa0b7e3793a654
Michael Madden <michaeltm98@gmail.con>
2019-01-20 10:33:07 -0500

    added username to campground

--- yelpcamp/routes/campgrounds.js
+++ yelpcamp/routes/campgrounds.js
@@ -17,29 +17,32 @@ router.get('/', function(req, res) {
 });
 
 //CREATE
-router.post('/', function(req, res) {
+router.post('/', isLoggedIn, function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description}
-    
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: description, author: author}
     Campground.create(newCampground, function(err, campground) {
         if (err) {
             console.log(err);
         } else {
-            console.log("Campground created")
+            console.log("Campground created: " + campground)
             res.redirect("/campgrounds");
         }
     })
 });
 
 //NEW
-router.get('/new', function(req, res) {
+router.get('/new', isLoggedIn, function(req, res) {
     res.render('campgrounds/new');
 });
 
 //SHOW
-router.get('/:id', function(req, res) {
+router.get('/:id', isLoggedIn, function(req, res) {
     var id = req.params.id;
     Campground.findById(req.params.id).populate("comments").exec(function(err, campground) {
         if (err) {
@@ -50,6 +53,13 @@ router.get('/:id', function(req, res) {
     });
     
 });
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    } 
+    res.redirect("/login");
+};
 
 
 module.exports = router;
